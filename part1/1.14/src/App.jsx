92a3c169e3d5994bd3bc009ef008a2d149e4b24a
anttont <anttoni.tornikoski@gmail.com>
2024-02-22 16:53:19 +0200

    Part 1 1.14 step 3
    Final commit

--- part1/1.14/src/App.jsx
+++ part1/1.14/src/App.jsx
@@ -10,6 +10,8 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
 
+
+
 const App = () => {
 
   const anecdotes = [
@@ -22,20 +24,49 @@ const App = () => {
     'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
     'The only way to go fast, is to go well.'
   ]
-   
+
+  const [votes, setVotes] = useState({ 0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0});
+
   const [selected, setSelected] = useState(0)
+  const [selectedMostVotes, setSelectedMostVotes] = useState(null)
 
   const handleNextClick = () => {
     setSelected(getRandomInt(anecdotes.length))
+    console.log(selected)
   }
+
+  const handleVoteClick = () => {
+    setVotes(votes => ({
+      ...votes,
+      [selected]: votes[selected] + 1
+    }));
+    setSelectedMostVotes(mostVoted({...votes, [selected]: votes[selected] + 1})); // Update the most voted anecdote
+  };
+  
+
+  const mostVoted = (votes) => {
+    let maxValue = 0;
+    let maxKey = -1; // Initialize with -1 instead of an empty string
+    for (const [key, value] of Object.entries(votes)) {
+      if (value > maxValue) {
+        maxValue = value;
+        maxKey = key;
+      }
+    }
+    return maxKey !== -1 ? parseInt(maxKey) : null; // Return null if no votes
+  };
+  
 
   return (
     <div>
       <div>
+        <h1>Anectode of the day</h1>
         <p>{anecdotes[selected]}</p>
         <Button handleClick={handleNextClick} text="Next" />
+        <Button handleClick={handleVoteClick} text="Vote" />
         
-        
+        <h1>Most voted anecdote</h1>
+        {selectedMostVotes !== null ? <p>{anecdotes[selectedMostVotes]}</p> : <p>No anecdote has been voted yet</p>}
         </div> 
     </div>
   )
