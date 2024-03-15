1c503d68f86117febe787052fe68314d042164ec
anttont <anttoni.tornikoski@gmail.com>
2024-03-15 20:12:32 +0200

    Part 3 esimerkki testaus

--- part3/index.js
+++ part3/index.js
@@ -1,151 +1,85 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require('express')
+const app = express()
+const cors = require('cors')
 
-const app = express();
+app.use(cors())
+app.use(express.static('dist'))
 
-// Define a custom token for Morgan to log request body
-morgan.token('body', (req, res) => {
-  return JSON.stringify(req.body);
-});
-
-// Define a custom token for Morgan to log request parameters
-morgan.token('params', (req, res) => {
-  return JSON.stringify(req.params);
-});
-
-// Use Morgan to log the request and include the custom token for the response body
-app.use(morgan(':method :url :status :response-time ms - response-body: :response-body'));
-
-
-app.use(express.json());
-
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method);
-  console.log('Path:  ', request.path);
-  console.log('Body:  ', request.body);
-  console.log('---');
-  next();
-};
-
-app.use(requestLogger);
-app.use((req, res, next) => {
-  const oldSend = res.send;
-
-  res.send = function (data) {
-    req.responseBody = data; 
-    oldSend.apply(res, arguments);
-  };
-
-  next();
-});
-
-
-morgan.token('response-body', (req, res) => {
-  
-  return req.responseBody;
-});
-
-let persons = [
+let notes = [
   {
-    name: 'Arto Helas',
-    number: '050',
-    id: '1',
+    id: 1,
+    content: "HTML is easy",
+    important: true
   },
   {
-    name: 'Ada Lovelace',
-    number: '39-44-5323523',
-    id: '2',
+    id: 2,
+    content: "Browser can execute only JavaScript",
+    important: false
   },
   {
-    name: 'Dan Abramov',
-    number: '12-43-234345',
-    id: '3',
-  },
-  {
-    name: 'Mary Poppendieck',
-    number: '39-23-6423122',
-    id: '4',
-  },
-];
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    important: true
+  }
+]
+
+app.use(express.json())
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>');
-});
+  response.send('<h1>Hello World!</h1>')
+})
 
-app.get('/api/persons', (request, response) => {
-  response.json(persons);
-});
-
-const PORT = 3001;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  const person = persons.find((person) => person.id === id);
-
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).end();
-  }
-});
-
-app.get('/api/info', (request, response) => {
-  const currentTime = new Date().toLocaleString();
-  const amountOfPeople = persons.length;
-  response.send(
-    `<div><h2>Amount of people: ${amountOfPeople}</h2>` +
-      `<h2>Current Time: ${currentTime}</h2><div/>`
-  );
-});
-
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  response.status(204).end();
-});
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
 
 const generateId = () => {
-  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
-  return maxId + 1;
-};
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body;
+app.post('/api/notes', (request, response) => {
+  const body = request.body
 
-  if (!body.name || !body.number) {
-    return response.status(400).json({
-      error: 'name or number is missing',
-    });
+  if (!body.content) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
   }
 
-  const nameExists = persons.some((person) => person.name === body.name);
-
-  if (nameExists) {
-    return response.status(400).json({
-      error: 'person is already present',
-    });
-  }
-
-  const person = {
+  const note = {
+    content: body.content,
+    important: body.important || false,
     id: generateId(),
-    name: body.name,
-    number: body.number,
-  };
+  }
 
-  persons = persons.concat(person);
+  notes = notes.concat(note)
 
-  response.json(person);
-});
+  response.json(note)
+})
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' });
-};
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    console.log('x')
+    response.status(404).end()
+  }
+})
 
-app.use(unknownEndpoint);
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
 
 
-
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
---
+++ part3/index2.js
@@ -0,0 +1,151 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+// Define a custom token for Morgan to log request body
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body);
+});
+
+// Define a custom token for Morgan to log request parameters
+morgan.token('params', (req, res) => {
+  return JSON.stringify(req.params);
+});
+
+// Use Morgan to log the request and include the custom token for the response body
+app.use(morgan(':method :url :status :response-time ms - response-body: :response-body'));
+
+
+app.use(express.json());
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:  ', request.path);
+  console.log('Body:  ', request.body);
+  console.log('---');
+  next();
+};
+
+app.use(requestLogger);
+app.use((req, res, next) => {
+  const oldSend = res.send;
+
+  res.send = function (data) {
+    req.responseBody = data; 
+    oldSend.apply(res, arguments);
+  };
+
+  next();
+});
+
+
+morgan.token('response-body', (req, res) => {
+  
+  return req.responseBody;
+});
+
+let persons = [
+  {
+    name: 'Arto Helas',
+    number: '050',
+    id: '1',
+  },
+  {
+    name: 'Ada Lovelace',
+    number: '39-44-5323523',
+    id: '2',
+  },
+  {
+    name: 'Dan Abramov',
+    number: '12-43-234345',
+    id: '3',
+  },
+  {
+    name: 'Mary Poppendieck',
+    number: '39-23-6423122',
+    id: '4',
+  },
+];
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>');
+});
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((person) => person.id === id);
+
+  if (person) {
+    response.json(person);
+  } else {
+    response.status(404).end();
+  }
+});
+
+app.get('/api/info', (request, response) => {
+  const currentTime = new Date().toLocaleString();
+  const amountOfPeople = persons.length;
+  response.send(
+    `<div><h2>Amount of people: ${amountOfPeople}</h2>` +
+      `<h2>Current Time: ${currentTime}</h2><div/>`
+  );
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body;
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({
+      error: 'name or number is missing',
+    });
+  }
+
+  const nameExists = persons.some((person) => person.name === body.name);
+
+  if (nameExists) {
+    return response.status(400).json({
+      error: 'person is already present',
+    });
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number,
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' });
+};
+
+app.use(unknownEndpoint);
+
+
+
