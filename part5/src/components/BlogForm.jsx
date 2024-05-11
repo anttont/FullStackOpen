e29ad7b26fc1b85b39ab506196c839741d60678e
anttont <anttoni.tornikoski@gmail.com>
2024-05-11 20:17:33 +0300

    part 5 playwright testien lisäys

--- part5/src/components/BlogForm.jsx
+++ part5/src/components/BlogForm.jsx
@@ -26,18 +26,21 @@ const BlogForm = ({ createBlog }) => {
       <form onSubmit={addBlog}>
         <p>Author</p>
         <input
+          data-testid='author-input'
           id="author-input"
           value={author}
           onChange={event => setAuthor(event.target.value)}
           placeholder='Whos the author?'
         />
         <input
+          data-testid='title-input'
           id="title-input"
           value={title}
           onChange={event => setTitle(event.target.value)}
           placeholder='Title of the blog?'
         />
         <input
+          data-testid='url-input'
           id="url-input"
           value={url}
           onChange={event => setUrl(event.target.value)}
