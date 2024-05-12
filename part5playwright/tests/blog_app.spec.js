324b2582504594ee9e5413015a2d7cf011615dc8
anttont <anttoni.tornikoski@gmail.com>
2024-05-12 21:09:24 +0300

    part5 osa 5.18

--- part5playwright/tests/blog_app.spec.js
+++ part5playwright/tests/blog_app.spec.js
@@ -12,3 +12,37 @@ test('login can be opened', async ({ page }) => {
   
 })
 
+describe('Blog app', () => {
+  beforeEach(async ({ page, request }) => {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
+      data: {
+        name: 'anton',
+        username: 'antto',
+        password: 'salainen'
+      }
+    })
+
+    await page.goto('')
+  })
+
+
+  test('user can login with correct credentials', async ({ page }) => {
+    await loginWith(page, 'antto', 'salainen')
+  
+    await expect(page.getByText('anton logged in')).toBeVisible()
+  })
+
+  
+  test('login fails with wrong password', async ({ page }) => {
+    await loginWith(page, 'antto', 'wrong')
+
+    const errorDiv = await page.locator('.error')
+    await expect(errorDiv).toContainText('Wrong credentials')
+    await expect(errorDiv).toHaveCSS('border-style', 'solid')
+    await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+    await expect(page.getByText('anton logged in')).not.toBeVisible()
+  })  
+
+  
+})
