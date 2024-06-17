6104f45e347815600eb0169244e3fd11ba8579ae
anttont <anttoni.tornikoski@gmail.com>
2024-06-17 18:06:36 +0300

    part 6 osa 6.10

--- part6/src/reducers/filterReducer.js
+++ part6/src/reducers/filterReducer.js
@@ -1,19 +1,18 @@
-const SET_FILTER = 'SET_FILTER'
+import { createSlice } from '@reduxjs/toolkit'
 
-export const setFilter = (filter) => {
-  return {
-    type: SET_FILTER,
-    payload: { filter }
+const initialState = ''
+
+const filterSlice = createSlice({
+  name: 'filter',
+  initialState,
+  reducers: {
+    setFilter(state, action) {
+      return action.payload
+    }
   }
-}
+})
 
-const filterReducer = (state = '', action) => {
-  switch (action.type) {
-    case SET_FILTER:
-      return action.payload.filter
-    default:
-      return state
-  }
-}
+export const { setFilter } = filterSlice.actions
 
-export default filterReducer
+export default filterSlice.reducer
+
